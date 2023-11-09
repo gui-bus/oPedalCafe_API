e5e837067dcad5f66571773e3961763299244b88
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-09 14:04:49 -0300

    feat: add category create controller/service

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,18 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
+// User Routes
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/userInfo", isAuthenticated, new DetailUserController().handle);
+
+// Category Routes
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
