95ed5380294dbbe024f7528177dff712ac2c8ec9
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-09 11:57:45 -0300

    feat: add CreateUserService

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,45 @@
+import { prismaClient } from "../../prisma";
+import { hash } from "bcryptjs";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ email, name, password }: UserRequest) {
+    if (!email) {
+      throw new Error("O email é obrigatório!");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("Já existe um usuário cadastrado com esse e-mail!");
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserService };
